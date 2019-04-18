8cfe713d861a65e4f0eae465f1a88378206e72d3
hyulin <hyulin@hotmail.com>
2019-04-18 23:41:43 +0900

    운영진 수정

--- Athena/System/CBotClient.cs
+++ Athena/System/CBotClient.cs
@@ -833,7 +833,6 @@ namespace Athena
                     strInfo += "건의사항, 문의사항이 있으실 때는\n";
                     strInfo += "(남)AWESOME(@leek603),\n";
                     strInfo += "(여)AWESOME(@AWESOMEYUU),\n";
-                    strInfo += "카페인(@sunho29),\n";
                     strInfo += "청포도(@leetk321),\n";
                     strInfo += "봄의캐롤(@rsmini) 에게\n";
                     strInfo += "문의해주세요.\n\n";
